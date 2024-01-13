be0489378820dc2d35bb089c8e9ff6441fe8c5cb
oybek777 <153380033+oybekvip777@users.noreply.github.com>
2024-01-13 19:47:55 +0500

    Add files via upload

---
+++ record1.16.py
@@ -0,0 +1,61 @@
+
+# 1-misol
+
+# class animal():
+#     def __init__(self,nema1,nema2,nema3):
+#         self.nema1=nema1
+#         self.nema2=nema2
+#         self.nema3=nema3
+
+#     def make(self):
+#         return self.nema1
+
+# obj1=animal('lion','bear','cat')
+# print()
+
+# 2-misol
+
+# 1-misolda
+
+
+# 3-misol     
+
+
+
+# class home(animal):
+#     def __init__(self,price,count):
+#         super().__init__(price,count)
+#         self.price=price
+#         self.count=count        
+
+#         return self.nema2
+
+    # @property
+    # def make(self):
+    #      return
+
+    # def add_nema(self,add_nomli)
+        # return self.make()
+
+# obj1=home(2000000,5)
+# print(obj1)
+
+
+# 4-misol        
+
+# 3-misolda
+
+# 5-misol
+        
+# 3-misolda
+
+
+
+
+
+
+
+
+
+
+
